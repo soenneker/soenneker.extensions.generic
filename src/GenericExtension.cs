450381d9560a289037a34e083f3da2420ad9e756
Jake Soenneker <jake@soenneker.com>
2023-06-01 12:55:10 -0500

    Somewhat built out

--- src/GenericExtension.cs
+++ src/GenericExtension.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using Soenneker.Extensions.Stream;
+using Soenneker.Utils.Json;
+using Soenneker.Utils.MemoryStream.Abstract;
 
-namespace Soenneker.Extensions.Generic
+namespace Soenneker.Extensions.Generic;
+
+public static class GenericExtension
 {
-    public static class GenericExtension
+    /// <summary>
+    /// Allows for feeding a stream into this (recommended via <see cref="IMemoryStreamUtil"/>), which gets serialized (JSON), and then the stream is returned
+    /// </summary>
+    public static System.IO.Stream ToStream<T>(this T input, System.IO.Stream stream)
     {
-        public static Stream ToStream<T>(this T input)
-        {
-            MemoryStream streamPayload = _memoryStreamUtil.GetSync();
-            _systemTextJsonSerializer.Serialize(streamPayload, input, typeof(T), default);
-            streamPayload.ToStart(); // Seek because Position set directly will lose the buffer: https://stackoverflow.com/a/71596118
-            return streamPayload;
-        }
+        JsonUtil.SerializeIntoStream(stream, input);
+        stream.ToStart();
+        return stream;
+    }
+
+    /// <summary>
+    /// Not recommended, builds a new <see cref="MemoryStream"/> and returns that after seeking to start.
+    /// </summary>
+    public static System.IO.Stream ToStream<T>(this T input)
+    {
+        var stream = new MemoryStream();
+        JsonUtil.SerializeIntoStream(stream, input);
+        stream.ToStart();
+        return stream;
     }
 }
