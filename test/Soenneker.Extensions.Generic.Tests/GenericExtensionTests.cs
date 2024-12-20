50fcd440355f42adc0163a248e3110921933d29b
Jake Soenneker <jake@soenneker.com>
2024-12-20 11:04:47 -0600

    Tests

---
+++ test/Soenneker.Extensions.Generic.Tests/GenericExtensionTests.cs
@@ -0,0 +1,13 @@
+using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Generic.Tests;
+
+public class GenericExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
