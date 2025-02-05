3a41ab04bcc6379340a8541cd5a0269a1f90248e
Jake Soenneker <jake@soenneker.com>
2025-02-05 15:03:40 -0600

    Build

--- src/GenericExtension.cs
+++ src/GenericExtension.cs
@@ -1,8 +1,9 @@
 ﻿using System.Diagnostics.Contracts;
 using System.IO;
+using System.Threading;
 using Soenneker.Extensions.Stream;
+using Soenneker.Extensions.Task;
 using Soenneker.Utils.Json;
-using Soenneker.Utils.MemoryStream.Abstract;
 
 namespace Soenneker.Extensions.Generic;
 
@@ -12,24 +13,28 @@ namespace Soenneker.Extensions.Generic;
 public static class GenericExtension
 {
     /// <summary>
-    /// Allows for feeding a stream into this (recommended via <see cref="IMemoryStreamUtil"/>), which gets serialized (JSON), and then the stream is returned
+    /// Allows for feeding a stream into this (recommended via IMemoryStreamUtil, which gets serialized (JSON), and then the stream is returned
     /// </summary>
     [Pure]
-    public static System.IO.Stream ToStream<T>(this T input, System.IO.Stream stream)
+    public static async System.Threading.Tasks.ValueTask<System.IO.Stream> ToStream<T>(this T input, System.IO.Stream stream, CancellationToken cancellationToken = default)
     {
-        JsonUtil.SerializeToStream(stream, input);
+        await JsonUtil.SerializeToStream(stream, input, cancellationToken: cancellationToken)
+                      .NoSync();
         stream.ToStart();
         return stream;
     }
 
     /// <summary>
-    /// Not recommended, builds a new <see cref="MemoryStream"/> and returns that after seeking to start.
+    /// Not recommended if you have access to IMemoryStreamUtil, builds a new <see cref="MemoryStream"/> and returns that after seeking to start.
     /// </summary>
     [Pure]
-    public static System.IO.Stream ToStream<T>(this T input)
+    public static async System.Threading.Tasks.ValueTask<MemoryStream> ToStream<T>(this T input, CancellationToken cancellationToken = default)
     {
         var stream = new MemoryStream();
-        JsonUtil.SerializeToStream(stream, input);
+
+        await JsonUtil.SerializeToStream(stream, input, cancellationToken: cancellationToken)
+                      .NoSync();
+
         stream.ToStart();
         return stream;
     }
