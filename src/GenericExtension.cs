bda389634ff9df80c07ae3d98cb8df1d5054ef20
Jake Soenneker <jake@soenneker.com>
2025-08-10 15:32:22 -0500

    ToBase64Json

--- src/GenericExtension.cs
+++ src/GenericExtension.cs
@@ -1,9 +1,13 @@
-﻿using System.Diagnostics.Contracts;
-using System.IO;
-using System.Threading;
-using Soenneker.Extensions.Stream;
+﻿using Soenneker.Extensions.Stream;
 using Soenneker.Extensions.Task;
 using Soenneker.Utils.Json;
+using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using Soenneker.Extensions.Arrays.Bytes;
+using Soenneker.Extensions.String;
 
 namespace Soenneker.Extensions.Generic;
 
@@ -37,5 +41,33 @@ public static class GenericExtension
 
         stream.ToStart();
         return stream;
+    }
+
+    /// <summary>
+    /// Serializes an object to JSON and encodes it as a Base64 string.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of object to serialize.
+    /// </typeparam>
+    /// <param name="obj">
+    /// The object to serialize and encode. Cannot be null.
+    /// </param>
+    /// <returns>
+    /// A Base64-encoded string representing the serialized JSON form of the object.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="obj"/> is null.
+    /// </exception>
+    /// <exception cref="JsonException">
+    /// Thrown if the object cannot be serialized to JSON.
+    /// </exception>
+    public static string ToBase64Json<T>(this T obj)
+    {
+        if (obj == null)
+            throw new ArgumentNullException(nameof(obj));
+
+        byte[] bytes = JsonUtil.SerializeToUtf8Bytes(obj);
+
+        return bytes.ToBase64String();
     }
 }
